5b0fa87528986ab5a1e69b9cb4becf646e3afa3f
Diogo Evaldt Webber <diogoevaldt17@gmail.com>
2025-04-01 13:50:00 -0300

    Teste2

--- OrganizadorEventos/Request/AddCostRequest.cs
+++ OrganizadorEventos/Request/AddCostRequest.cs
@@ -2,6 +2,6 @@ namespace OrganizadorEventos.Request;
 
 public class AddCostRequest
 {
-    public string Description { get; set; } = string.Empty;
+    public string Description { get; set; }
     public decimal Amount { get; set; }
 }
